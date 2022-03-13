1c3e6c647e77ffafcf97424f1975bf7fbbda0238
puerarongo <akhardin96@gmail.com>
2022-03-13 14:58:05 +0300

    Working with the main functionality

--- src/index.js
+++ src/index.js
@@ -1,36 +1,64 @@
 import "./css/styles.css";
 import { fetchCards } from './js_function/fetchCards';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 const formEl = document.querySelector(".search-form");
 const inputEl = document.querySelector(".form__input");
 const galleryEl = document.querySelector(".gallery");
+const moreEl = document.querySelector(".load-more");
 //const buttonEl = document.querySelector(".form__button");
 
 
 let inputValue = null;
+let saveValue = null;
+let count = 0;
+
+
+
+// ! FORM FUNCTION
 
 const inputHandler = (event) => {
     inputValue = event.target.value;
 };
 
 const submitHandler = (event) => {
-    event.preventDefault();
-    
-    funcForPromise(inputValue);
+  event.preventDefault();
+  count += 1;
+
+  if (saveValue !== inputValue) {
+    newQ();
+  }
+  
+  if (count > 1) {
+    return console.log(26)
+  }
+  
+  moreEl.style.display = "inline";
+  funcForPromise(inputValue, count);
+  saveValue = inputValue;
+};
+
+const searchMore = () => {
+  count += 1;
+
+  funcForPromise(saveValue, count);
 };
 
 
-const funcForPromise = (name) => {
-    fetchCards(name)
+const funcForPromise = (name, counter) => {
+    fetchCards(name, counter)
         .then(result => cardConstructor(result))
         .catch(error => Notify.failure("Sorry, there are no images matching your search query. Please try again."))           
 };
 
 
+
+// ! ADDITIONAL FUNCTION
 const cardConstructor = (value) => {
-    console.log(value.hits);
-    let makeCard = value.hits.map((elem) => `<div class="photo-card">
+  console.log(value);
+  hitCheck(value);
+  let makeCard = value.hits.map((elem) => `<div class="photo-card">
   <img src="${elem.webformatURL}" alt="${elem.tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
@@ -48,9 +76,36 @@ const cardConstructor = (value) => {
   </div>
 </div>`).join("");
     
-    galleryEl.insertAdjacentHTML("beforeend", makeCard);
+  galleryEl.insertAdjacentHTML("beforeend", makeCard);
 };
 
 
+const newQ = () => {
+  count = 1;
+
+  moreEl.style.display = "none";
+  galleryEl.innerHTML = "";
+}
+
+const hitCheck = (hits) => {
+  if (count === 1) {
+    Notify.success(`Hooray! We found ${hits.totalHits} images.`)
+  }
+
+  if (hits.hits.length < 40) {
+    moreEl.style.display = "none";
+    Report.warning(
+      "Warning!",
+      "We're sorry, but you've reached the end of search results.",
+      "Ok",
+    );
+  }
+};
+
+
+
+
+// todo Actions
 inputEl.addEventListener("input", inputHandler);
 formEl.addEventListener("submit", submitHandler);
+moreEl.addEventListener("click", searchMore)
--- src/js_function/fetchCards.js
+++ src/js_function/fetchCards.js
@@ -1,10 +1,12 @@
 const BASE_CASE = "https://pixabay.com/api/?";
 const API_KEY = "26014501-193a5d23d0126e5770d29076d";
 
-export const fetchCards = async (name) => {
+export const fetchCards = async (name, counter) => {
     const responce = await fetch(`${BASE_CASE}key=${API_KEY}&q=${name}
-    &image_type=photo&orientation=horizontal&safesearch=true`);
+    &image_type=photo&orientation=horizontal&safesearch=true&page=${counter}&per_page=40`);
     const cards = await responce.json();
+
+    console.log(cards.hits.length)
     if (cards.total !== 0) {
         return cards;
     }
